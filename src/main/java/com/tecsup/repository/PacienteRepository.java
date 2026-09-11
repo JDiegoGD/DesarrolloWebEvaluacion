8983ca509e643d92f1e0f47c9ee8ad072baa820d
AntonioPromaweb <vasquezfloresluisantonio33@gmail.com>
2026-09-11 17:30:47 -0500

    agregar PacienteRepository para gestion de datos

--- src/main/java/com/tecsup/repository/PacienteRepository.java
+++ src/main/java/com/tecsup/repository/PacienteRepository.java
@@ -1,4 +1,9 @@
 package com.tecsup.repository;
 
-public class PacienteRepository {
+import com.tecsup.model.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 }
