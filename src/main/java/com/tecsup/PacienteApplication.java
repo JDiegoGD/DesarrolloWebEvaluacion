0995524e2e5851e23484a5e3bb0f724b1c875d85
Juan Diego <jdgddiego@gmail.com>
2026-09-11 16:29:32 -0500

    inicial

--- src/main/java/com/tecsup/PacienteApplication.java
+++ src/main/java/com/tecsup/PacienteApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PacienteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PacienteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PacienteApplication.class, args);
+    }
 
 }
--- src/test/java/com/tecsup/PacienteApplicationTests.java
+++ src/test/java/com/tecsup/PacienteApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PacienteApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
