51314e4bad4b843466f3bbfaea14cf7df91e1119
Juan Diego <juan.gutierrez.d@tecsup.edu.pe>
2026-09-12 16:04:43 -0500

    Definicion de Modelos Ubigeo y TipoDocumento.

--- src/main/java/com/tecsup/model/TipoDocumento.java
+++ src/main/java/com/tecsup/model/TipoDocumento.java
@@ -1,4 +1,21 @@
 package com.tecsup.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "tipo_documento")
 public class TipoDocumento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idTipoDocumento;
+
+    @Column(nullable = false, length = 50)
+    private String nombre;
+
+    @OneToMany(mappedBy = "tipoDocumento")
+    private List<Paciente> pacientes;
 }
--- src/main/java/com/tecsup/model/Ubigeo.java
+++ src/main/java/com/tecsup/model/Ubigeo.java
@@ -1,4 +1,18 @@
 package com.tecsup.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "ubigeo")
 public class Ubigeo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idUbigeo;
+
+    @Column(nullable = false, length = 100) private String distrito;
+    @Column(nullable = false, length = 100) private String provincia;
+    @Column(nullable = false, length = 100) private String departamento;
 }
