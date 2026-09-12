064e1db39bd6ff48afd6ecc882a075c3da5ca953
Luis Maldonado <luis.maldonado@tecsup.edu.pe>
2026-09-12 15:45:03 -0500

    Revert "RF-PAC-09: El sistema deberá permitir actualizar la información del seguro."
    This reverts commit d6ebf7b231b2e55f9518693f7605218d059e7741.

--- src/main/java/com/tecsup/model/ContactoEmergencia.java
+++ src/main/java/com/tecsup/model/ContactoEmergencia.java
@@ -1,25 +1,4 @@
 package com.tecsup.model;
 
-import jakarta.persistence.*;
-import lombok.Data;
-
-@Data
-@Entity
-@Table(name = "contacto_emergencia")
 public class ContactoEmergencia {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer idContacto;
-
-    @ManyToOne
-    @JoinColumn(name = "id_paciente", nullable = false)
-    private Paciente paciente;
-
-    @Column(nullable = false, length = 150) private String nombreCompleto;
-    @Column(length = 50)  private String parentesco;
-    @Column(length = 20)  private String telefono;
-    @Column(length = 200) private String direccion;
-    @Column(length = 100) private String correoElectronico;
-    @Column(nullable = false) private Boolean esPrincipal = false;
 }
